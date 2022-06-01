14dbe224b67f5fcf32c3373a63a78e9fcf06861a
Suba <suba1105usui@gmail.com>
2022-06-02 01:27:40 +0900

    Add Settings Panel

---
+++ Main.js
@@ -0,0 +1,15 @@
+function set(num) {
+    var ret;
+    if (num < 10) { ret = "0" + num; }
+    else { ret = num; }
+    return ret;
+}
+function showClock() {
+    var nowTime = new Date();
+    var nowHour = set(nowTime.getHours());
+    var nowMin = set(nowTime.getMinutes());
+    var nowSec = set(nowTime.getSeconds());
+    var msg = nowHour + ":" + nowMin + ":" + nowSec;
+    document.getElementById("realtime").innerHTML = msg;
+}
+setInterval("showClock()", 500);
