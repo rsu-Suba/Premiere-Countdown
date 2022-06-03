9a3b47a7ef4298c1a6795bb12d71c39c4d649931
Suba <suba1105usui@gmail.com>
2022-06-04 00:51:49 +0900

    Editing Settings Button

--- Main.js
+++ Main.js
@@ -13,3 +13,24 @@ function showClock() {
     document.getElementById("realtime").innerHTML = msg;
 }
 setInterval("showClock()", 500);
+
+document.getElementById("hidden_box").style.visibility = "hidden";
+const settingsToggle = document.getElementById("set-text");
+
+settingsToggle.addEventListener("change",function(e){
+    if (e.target.checked){
+        settingsMode('open');
+    } else {
+        settingsMode('close');
+    }
+})
+
+function settingsMode(mode){
+    var settingsObj = document.getElementById("hidden_box");
+    if (mode == 'open'){
+        settingsObj.style.visibility = "visible";
+    }
+    else if (mode == 'close'){
+        settingsObj.style.visibility = "hidden";
+    }
+}
